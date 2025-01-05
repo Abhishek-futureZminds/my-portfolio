6e4454ea24d4fd5ab4db67b836a15a35b31ea925
abhi88000 <abhishek@futurezminds.in>
2025-01-05 14:46:33 +0530

    Removed default and added Home

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
-import Navbar from './Navbar';
-import viteLogo from '/vite.svg';
-import reactLogo from './assets/react.svg';
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
 import './App.css'
 
 function App() {
@@ -10,23 +9,8 @@ function App() {
   return (
     <>
       <Navbar />
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR updates.
-        </p>
-      </div>
+      <Home />
+
     </>
   );
 }
---
+++ my-portfolio/src/Components/Home.jsx
@@ -0,0 +1,10 @@
+function Home () {
+  return (
+    <div>
+      <h1>Welcome to my portfolio!</h1>
+      <p>Feel free to browse around and learn more about me.</p>
+    </div>
+  );
+}
+
+export default Home;
