5e80a85bedda8165513a94a54df5093736f31c7c
abhi88000 <abhishek@futurezminds.in>
2025-01-18 16:09:37 +0530

    Added footer

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -1,13 +1,16 @@
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import './App.css'
+import Footer from './Components/Footer';
 
 function App() {
 
   return (
     <>
       {/* <Navbar /> */}
+      <Footer />
       <Home d={5} m={8} y={2021} /> 
+      
     </>
   );
 }
